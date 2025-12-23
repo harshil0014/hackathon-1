fa17a513be212842f7aa4d86a021c5a62c3d9dda
Harshil Vora <harshilvora206@gmail.com>
2025-12-23 04:04:32 -0800

    Add MongoDB connection via mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
+const { PORT } = require('./config/env');
+const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
